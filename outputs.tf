27982f98eecf3915563f629fc57e9ed16c82524f
Chris Tippett <c.tippett@gmail.com>
2020-06-15 07:27:16 +1000

    docs: add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "instance_name" {
-  value = var.name
+  description = "The instance name for the PgBouncer instance."
+  value       = var.name
 }
 
 output "private_ip_address" {
-  value = local.private_ip
+  description = "The first private (PRIVATE) IPv4 address assigned for the master instance."
+  value       = local.private_ip
 }
 
 output "public_ip_address" {
-  value = local.public_ip
+  description = "The first public (PRIMARY) IPv4 address assigned for the master instance."
+  value       = local.public_ip
 }
 
 output "port" {
-  value = var.port
+  description = "The port number PgBouncer listens on."
+  value       = var.port
 }
